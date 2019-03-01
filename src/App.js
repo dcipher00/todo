cd0519f7b9055f90eeb5401feec8162c869d950e
Gaurav Mishra <gaurav.mishra2050@gmail.com>
2019-03-02 05:19:29 +0530

    simple todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Note from './components/Note';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      noteText: '',
+      notes: [],
+    }
+  }
+
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value})
+  }
+
+  handleKeyPress = (event) => {
+    if(event.key === 'Enter') {
+
+    }
+  }
+
+  deleteNote(index) {
+    let notesarr = this.state.notes;
+    notesarr.splice(index, 1);
+    this.setState({ notes: notesarr })
+  }
+
+  addNote() {
+    if(this.state.noteText === '') {return}
+    let notesarr = this.state.notes;
+    notesarr.push(this.state.noteText);
+    this.setState({ noteText: ''});
+    this.textInput.focus();
+  }
+
   render() {
+    let notes = this.state.notes.map((val, key) => {
+      return <Note key={key} text={val}
+        deleteMethod={ () => this.deleteNote(key) } />
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Container">
+      <div className="header">Todo App</div>
+      {notes}
+      <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+      <input type="text"
+        ref={((input) => {this.textInput = input})}
+        className="textInput"
+        value={this.state.noteText}
+        onChange={noteText => this.updateNoteText(noteText)}
+        onKeyPress={this.handleKeyPress.bind(this)}
+        />
       </div>
     );
   }
